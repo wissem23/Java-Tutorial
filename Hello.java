31f6e8dd89be43ba17098976e6d68cf1c6bfe983
wissem <wissem.chehoumi@esprit.tn>
2024-07-16 22:00:15 +0100

    Logical Operators

--- Hello.java
+++ Hello.java
@@ -1,18 +1,26 @@
 class Hello {
-    public static void main( String args[]){
-        int i = 12 ;
-        byte b = 127;
+    public static void main( String[] args){
+        // int i = 12 ;
+        // byte b = 127;
         
-        i = b;       // conversion
-        b = (byte)i; //casting 
-        // type promotions
-        byte x = 30;
-        byte y = 10;
+        // i = b;       // conversion
+        // b = (byte)i; //casting 
+        // // type promotions
+        // byte x = 30;
+        // byte y = 10;
 
-        //byte z = x*y ; out of byte range
-        int z = x*y ;
+        // //byte z = x*y ; out of byte range
+        // int z = x*y ;
+
+        int a = 10;
+        int b = 5;
+
+        int x = 100;
+        int y = 50;
+
+        boolean r = a<b && x<y;
 
 
-        System.out.println(z);
+        System.out.println(r);
     }
 }
