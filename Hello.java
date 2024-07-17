a9779a653bf40b90b75dde38674c500a6fb6eb44
wissem <wissem.chehoumi@esprit.tn>
2024-07-17 01:55:19 +0100

    while loop

--- Hello.java
+++ Hello.java
@@ -50,33 +50,63 @@ class Hello {
 
         //switch
 
-        int n = 5;
-        switch (n) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Thursday");
-                break;
-            case 4:
-                System.out.println("Wednesday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Sturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
+        // int n = 5;
+        // switch (n) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Sturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
         
-            default:
-                System.out.println("enter a valid number");
-                break;
-        }
+        //     default:
+        //         System.out.println("enter a valid number");
+        //         break;
+        // }
+
+
+        // while loop
+        // int n = 0;
+        // while (n<10) {
+        //     System.out.println(n);
+        //     n++;            
+        // }
+
+        // Do while loop
+        // int n = 11;
+        // do {
+        //     System.out.println(n);
+        //     n++;
+        // } while (n<10);
+
+        // nested while loop
+        // int i = 0;
+        // while (i<10) {
+        //     System.out.println(i);
+        //     int j = 0;
+        //     while (j<3) {
+        //         System.out.println("done "+j);
+        //         j++;
+        //     }
+        //     i++;            
+        // }
+
+
+
     }
 }
