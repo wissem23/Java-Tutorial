35a4f17c67505445ff65a2a098676652fcfa12ff
wissem <wissem.chehoumi@esprit.tn>
2024-07-17 20:00:25 +0100

    for loop

--- Hello.java
+++ Hello.java
@@ -106,6 +106,12 @@ class Hello {
         //     i++;            
         // }
 
+        //  for loop
+
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
 
 
     }
